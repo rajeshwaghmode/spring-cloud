aec7c3808c406cbdef8a6ae7b9f2b218a86274c6
Rajesh Waghmode <rajes.waghmode.professional@outlook.com>
2022-06-20 19:43:57 +0530

    Gateway changes

---
+++ microservices/api-gateway/src/main/java/com/java/learning/spring/cloud/microservices/apigateway/configuration/RoutConfiguration.java
@@ -0,0 +1,23 @@
+package com.java.learning.spring.cloud.microservices.apigateway.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                    .route(p -> p.path("/currency-conversion/**")
+                            .uri("lp://currency-conversion-service/"))
+                    .route(p -> p.path("/currency-conversion-feign/**")
+                            .uri("lp://currency-conversion-service"))
+                    .route(p -> p.path("/currency-exchange/**")
+                            .uri("lp://currency-exchange-service"))
+                .build();
+    }
+}
